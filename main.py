b583885573ab90e0d7ce647d2e858d84f2a8b9f0
bigwoodhead <necessity@126.com>
2021-08-17 00:02:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,53 +7,57 @@ import re
 signIn = {'username': os.environ["USERNAME"], #学号
           'password': os.environ["PASSWORD"]} #登陆密码
 
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Mobile Safari/537.36',
 }
 
 conn = requests.Session()
-signInResponse= conn.post(
-    url="https://app.upc.edu.cn/uc/wap/login/check",
+signInResponse = conn.post(
+    url="https://move.muc.edu.cn/uc/wap/login/check",
     headers=headers,
     data= signIn, 
     timeout=10
 )
 
 historyResponse = conn.get(
-    url="https://app.upc.edu.cn/ncov/wap/default/index?from=history",
+    url="https://move.muc.edu.cn/ncov/wap/default/index?from=history",
     headers=headers,
     data={'from': 'history'},
     timeout=10
 )
+
 historyResponse.encoding = "UTF-8"
 
 html = lxml.html.fromstring(historyResponse.text)
+
 JS = html.xpath('/html/body/script[@type="text/javascript"]')
 JStr = JS[0].text
+
 default = re.search('var def = {.*};',JStr).group()
 oldInfo = re.search('oldInfo: {.*},',JStr).group()
 
-firstParam = re.search('sfzgsxsx: .,',JStr).group()
-firstParam = '"' + firstParam.replace(':','":')
-secondParam = re.search('sfzhbsxsx: .,',JStr).group()
-secondParam = '"' +  secondParam.replace(':','":')
-lastParam = re.search('szgjcs: \'(.*)\'',JStr).group()
-lastParam = lastParam.replace('szgjcs: \'','').rstrip('\'')
 
+Param = '"geo_api_info": { "type": "complete", "info": "SUCCESS", "status": 1, "position": {"O": 116.3270901582031, "P": 40.02177467243305, "lng": 116.3270901582031, "lat": 40.02177467243305}}'
+        
+           
 newInfo = oldInfo
-newInfo = newInfo.replace('oldInfo: {','{' + firstParam + secondParam).rstrip(',')
+newInfo = newInfo.replace('oldInfo: {','{' + Param + ',').rstrip(',')
 
 defaultStrip = default.replace('var def = ','').rstrip(';')
 defdic = json.loads(defaultStrip)
 
+
 dic = json.loads(newInfo)
 dic['ismoved'] = '0'
+
 for j in ["date","created","id","gwszdd","sfyqjzgc","jrsfqzys","jrsfqzfy"]:
     dic[j] = defdic[j]
-dic['szgjcs'] = lastParam
+    
+dic['szgjcs'] = Param
 
 saveResponse = conn.post(
-    url="https://app.upc.edu.cn/ncov/wap/default/save",
+    url="https://move.muc.edu.cn/ncov/wap/default/save",
     headers=headers,
     data = dic,
     timeout=10
